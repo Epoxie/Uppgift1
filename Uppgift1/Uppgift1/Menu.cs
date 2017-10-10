a37a6149845537093780b32f501d21bbfebf175d
Magnus <Olchardwell@hotmail.com>
2017-10-10 15:08:58 +0200

    started with dynamic pages

--- Uppgift1/Uppgift1/Menu.cs
+++ Uppgift1/Uppgift1/Menu.cs
@@ -9,28 +9,31 @@ namespace Uppgift1
     class Menu
     {
         String input = "";
+        Page CurrentPage;
+
+        public Menu()
+        {
+            CurrentPage = new Page("1. Search for items\n" +
+                                   "2. View the stock\n" +
+                                   "3. View the cart\n" +
+                                   "4. Checkout");
+        }
 
         public void Start()
         {
             while (true)
             {
-                Console.WriteLine("Choose what to do by typing a number:");
+                Console.WriteLine("Choose what to do by typing a number:"); // writes out the first line
+                CurrentPage.WritePage(); // writes out the meat in the menu
+                Console.WriteLine("0. Exit program"); // writes the ending line
                 input = Console.ReadLine();
                 Console.Clear();
                 if (CheckInput(input))
                 {
-                    switch (input[0])
-                    {
-                        case '1':
-                            Console.WriteLine("Case 1");
-                            break;
-                        case '2':
-                            Console.WriteLine("Case 2");
-                            break;
-                        case '0':
-                            Environment.Exit(0);
-                            break;
-                    }
+                    if (input[0] == '0')
+                        Environment.Exit(0);
+                    else
+                        CurrentPage.Action(input[0]);
                 }
                 else
                     Console.WriteLine("Wrong input, please type one of the numbers in the list.");
---
+++ Uppgift1/Uppgift1/Page.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1
+{
+    class Page
+    {
+        String pageInfo;
+
+        public Page(String pageInfo)
+        {
+            this.pageInfo = pageInfo;
+        }
+
+        public void WritePage()
+        {
+            Console.WriteLine(pageInfo);
+        }
+
+        public void Action(char input)
+        {
+            // do something
+        }
+    }
+}
